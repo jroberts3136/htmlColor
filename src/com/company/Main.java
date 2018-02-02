ea67ac9b52cda285e4f7acea63025a05039e54ab
jroberts3136 <Jroberts3136@gmail.com>
2018-02-02 08:51:06 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+	// name and hex in colors.txt from jdurrell
+        Scanner fileReader = new Scanner(new File("colors.txt"));
+    }
+}
