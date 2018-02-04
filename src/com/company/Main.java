f9f5914b5c52f0db7b089b5d08006e249faf0eaf
jroberts3136 <jroberts3136@gmail.com>
2018-02-04 12:50:05 -0500

    More RGB codes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,13 @@ public class Main {
     public static void main(String[] args) throws IOException{
 	// name and hex in colors.txt from jdurrell
         Scanner fileReader = new Scanner(new File("colors.txt"));
+
+        String [] htmlNames = new String [148];
+        String [] hexValues = new String [148];
+        String [] rValues = new String [148];
+        String [] gValues = new String [148];
+        String [] bValues = new String [148];
+
+
     }
 }
